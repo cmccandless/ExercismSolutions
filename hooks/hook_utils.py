78233b046f5a2d5419d053f21d7a9430ef3da5f9
Corey McCandless <crm1994@gmail.com>
2017-10-26 10:19:05 -0500

    Add configuration for Travis-CI

--- hooks/hook_utils.py
+++ hooks/hook_utils.py
@@ -1,12 +1,18 @@
 from io import StringIO
+import os
 import subprocess as sp
 import sys
 
 
 def log(msg, label=None):
+    try:
+        import __main__
+        main_file = __main__.__file__
+    except AttributeError:
+        main_file = '<SHELL>'
     if label is not None:
         msg = '[{}] {}'.format(label, msg)
-    print('pre-commit: {}'.format(msg), flush=True)
+    print('{}: {}'.format(main_file, msg), flush=True)
 
 
 def abort(msg='', label=None):
@@ -28,6 +34,17 @@ def exec(*args, quiet=True, label=None, shell=False):
         abort('[{}] Not found: {}'.format(' '.join(args), e.filename), label)
 
 
+def linter(track, quiet=True):
+    shell = False
+    if track in ['python', 'hooks']:
+        args = ['flake8', track]
+    elif track == 'haskell':
+        args = ['hlint', track]
+    else:
+        return ('No available linter for "{}"'.format(track), 0)
+    return exec(*args, quiet=quiet, label=track, shell=shell)
+
+
 def runner(track, exercise, quiet=True):
     label = '{}/{}'.format(track, exercise)
     shell = False
@@ -45,7 +62,9 @@ def runner(track, exercise, quiet=True):
     elif track == 'python':
         args = ['pytest', '-q']
     elif track == 'ruby':
-        args = ['ruby', '{}_test.rb'.format(exercise)]
+        args = ['ruby', '{}_test.rb'.format(exercise.replace('-', '_'))]
+    else:
+        return ('unknown track "{}"'.format(track), -1)
     return exec(*args, quiet=quiet, label=label, shell=shell)
 
 
@@ -59,8 +78,26 @@ def get_current_branch():
     return branch
 
 
-def get_changed_exercises():
-    changes, ret = exec('git', 'diff', '--cached', '--name-status')
+def get_exercises(track):
+    ignore = {'__pycache__', '.vs', '.vscode', 'bin', 'obj', 'Properties'}
+    results = []
+    base_path = os.path.join(os.getcwd(), track)
+    for exercise in os.listdir(base_path):
+        if exercise in ignore:
+            continue
+        if os.path.isdir(os.path.join(base_path, exercise)):
+            results.append(exercise)
+    return results
+
+
+def get_changed_exercises(commit=-1):
+    args = ['git', 'diff', '--name-status']
+    if commit < 0:
+        args.append('--cached')
+    else:
+        args.append('HEAD~{}'.format(commit + 1))
+        args.append('HEAD~{}'.format(commit))
+    changes, ret = exec(*args)
     changes = changes.split('\n')
     if ret != 0:
         abort('error {} occurred'.format(ret), 'git-diff')
@@ -75,3 +112,8 @@ def get_changed_exercises():
         track, exercise, *_ = filename.split('/')
         changed_exercises.add((track, exercise))
     return changed_exercises
+
+
+def get_git_root():
+    git_root, ret = exec('git rev-parse --show-toplevel')
+    return git_root if ret == 0 else None
